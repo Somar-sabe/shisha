1339b5ca6253aaf2c3be9e0155fb75069ba0307d
tech-aatienda <techteam@aatienda.com>
2024-11-08 16:18:06 +0400

    splsfsfff

--- src/components/elements/MetaTags.jsx
+++ src/components/elements/MetaTags.jsx
@@ -9,7 +9,11 @@ const MetaTags = ({title}) => {
             <meta httpEquiv="x-ua-compatible" content="ie=edge" />
             <meta name="description" content="eTrade eCommerce React Next JS Template" />
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-            <title>{`${title?title:"eTrade"} - eCommerce React Next JS Template`}</title>
+            <title>{`${title?title:"Shisha"}Holster UAE - Premium Tobacco, Premium Moments`}</title>
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         </Head>
      );
 }
