6131aefdfec5dce06f91d6d5b55a95ef7657bd04
tech-aatienda <techteam@aatienda.com>
2024-11-08 16:31:21 +0400

    splsfsll;k;fff

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,6 +18,16 @@ const  RootLayout = ({ children }) => {
 			<link rel="preconnect" href="https://fonts.googleapis.com" />
 			<link rel="preconnect" href="https://fonts.gstatic.com" />
 			<link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400&display=swap" rel="stylesheet" />
+			            <meta charSet="utf-8" />
+            <meta name="robots" content="noindex, follow" />
+            <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+            <meta name="description" content="eTrade eCommerce React Next JS Template" />
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+            <title>Shisha Holster UAE - Premium Tobacco, Premium Moments</title>
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 			</head>
 			<body>
 				<Providers>
