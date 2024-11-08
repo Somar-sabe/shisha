9fbb3583955841833de7c79b5350a23b84896382
tech-aatienda <techteam@aatienda.com>
2024-11-08 23:07:09 +0400

    spldlfjeiuddmlsfj';;f

--- src/data/Footer.js
+++ src/data/Footer.js
@@ -71,7 +71,7 @@ const FooterData = {
 
 
     footerInfo: {
-        address: "B2B Tower - Office 1602, <br /> Dubai, Buissness bay, <br /> UAE.",
+        address: "B2B Tower - Office 1602, <br /> Dubai, Buissness bay,UAE.",
         email: "Info@holster-tobacco.com",
         phone: "+971 55 429 1019"
     },
