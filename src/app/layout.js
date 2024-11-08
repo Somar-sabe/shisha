f2b11fb4e9a0b0a1ea28ce7737a7fe4fe03a5555
tech-aatienda <techteam@aatienda.com>
2024-11-08 23:10:00 +0400

    spldlfjeiuddmlsfjl;kp';;f

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,7 @@ const  RootLayout = ({ children }) => {
             <meta httpEquiv="x-ua-compatible" content="ie=edge" />
             <meta name="description" content="eTrade eCommerce React Next JS Template" />
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-            <title>Shisha Holster UAE - Premium Tobacco, Premium Moments</title>
+            <title>{`Holster UAE - Premium Tobacco, Premium Moments`}</title>
             <link rel="icon" href="/favicon.ico" />
             <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
