ef87af3599c8217f944c164abd106f337ff979e2
tech-aatienda <techteam@aatienda.com>
2024-11-09 17:09:19 +0400

    spldlfjeiuddmlsfjl;p'kkp'klkh;;f

--- src/data/Team.js
+++ src/data/Team.js
@@ -2,19 +2,15 @@ const TeamData = [
     {
         thumbnail: "/images/team/Group 22-min.png",
         designation: "Founder",
-        name: "Ninur Akjul"
+        name: "Ninor Akgul"
     },
     {
         thumbnail: "/images/team/Group 25-min.png",
         
         designation: "UAE General Director",
-        name: "Didar chulpayiev"
+        name: "Didar Chulbayev"
     },
-    {
-        thumbnail: "/images/team/Group 23-min.png",
-        designation: "Marketing Director",
-        name: "Valerya Weber"
-    },
+
    
 ]
 
