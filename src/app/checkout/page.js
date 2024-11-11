966c4b401819904d5bba2d4b8e40eb2f9956a796
tech-aatienda <techteam@aatienda.com>
2024-11-11 18:16:01 +0400

    skjgskg;d;dfs'kflks'gdsf'rl.afsfsd;j

--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -76,7 +76,7 @@ const Checkout = () => {
                     callback_url: "https://shisha-zeta.vercel.app/checkout/order-received",
                 };
 
-                const response = await fetch("https://ziina-payment-api-url.com/payment_intent", {
+                const response = await fetch("https://api.ziina.com/v1/payments", {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json",
