6b719457dd1204a7fc75d2fa58e90dfa4ebd41fb
tech-aatienda <techteam@aatienda.com>
2024-11-13 12:23:53 +0400

    syurysapfiiposdsaaaawsasdr

--- src/app/shop/ShopWithSidebar.js
+++ src/app/shop/ShopWithSidebar.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect, Suspense } from "react";
+import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import Section from "@/components/elements/Section";
 import ProductsData from "@/data/Products";
@@ -36,46 +36,44 @@ const ShopWithSidebar = () => {
     };
 
     return ( 
-        <Suspense fallback={<div>Loading...</div>}>
-            <Section pClass="axil-shop-area">
-                <div className="row">
-                    <div className="col-lg-3">
-                        <div className="axil-shop-sidebar">
-                            <div className="d-lg-none">
-                                <button className="sidebar-close filter-close-btn"><i className="fas fa-times" /></button>
-                            </div>
-                            {/* Category Filter */}
-                            <div className={`toggle-list product-categories ${cateToggle ? "active" : ""}`}>
-                                <h6 onClick={() => setCateToggle(!cateToggle)} className="title">CATEGORIES</h6>
-                                {cateToggle && 
-                                    <div className="shop-submenu">
-                                        <ul>
-                                            {Category.map((data, index) => (
-                                                <li className={filterText === slugify(data.cate) ? "current-cat" : ""} key={index}>
-                                                    <button onClick={() => categoryHandler(slugify(data.cate))}>{data.cate}</button>
-                                                </li>
-                                            ))}
-                                        </ul>
-                                    </div>
-                                }
-                            </div> 
+        <Section pClass="axil-shop-area">
+            <div className="row">
+                <div className="col-lg-3">
+                    <div className="axil-shop-sidebar">
+                        <div className="d-lg-none">
+                            <button className="sidebar-close filter-close-btn"><i className="fas fa-times" /></button>
                         </div>
-                    </div>
-                    <div className="col-lg-9">
-                        <div className="row row--15">
-                            {filterProduct.length > 0 ? filterProduct.slice(0, productShow).map((data) => (
-                                <div className="col-xl-4 col-sm-6" key={data.id}>
-                                    <ProductSeven product={data} pClass="mb--30" />
+                        {/* Category Filter */}
+                        <div className={`toggle-list product-categories ${cateToggle ? "active" : ""}`}>
+                            <h6 onClick={() => setCateToggle(!cateToggle)} className="title">CATEGORIES</h6>
+                            {cateToggle && 
+                                <div className="shop-submenu">
+                                    <ul>
+                                        {Category.map((data, index) => (
+                                            <li className={filterText === slugify(data.cate) ? "current-cat" : ""} key={index}>
+                                                <button onClick={() => categoryHandler(slugify(data.cate))}>{data.cate}</button>
+                                            </li>
+                                        ))}
+                                    </ul>
                                 </div>
-                            )) : <h4 className="text-center pt--30">No Product Found</h4>}
-                        </div>
-                        <div className="text-center pt--20">
-                            <button className={`axil-btn btn-bg-lighter btn-load-more ${filterProduct.length < productShow ? "disabled" : ""}`} onClick={ProductShowHandler}>{filterProduct.length < productShow ? "No More Data" : "Load more"}</button>
-                        </div>
+                            }
+                        </div> 
                     </div>
                 </div>
-            </Section>
-        </Suspense>
+                <div className="col-lg-9">
+                    <div className="row row--15">
+                        {filterProduct.length > 0 ? filterProduct.slice(0, productShow).map((data) => (
+                            <div className="col-xl-4 col-sm-6" key={data.id}>
+                                <ProductSeven product={data} pClass="mb--30" />
+                            </div>
+                        )) : <h4 className="text-center pt--30">No Product Found</h4>}
+                    </div>
+                    <div className="text-center pt--20">
+                        <button className={`axil-btn btn-bg-lighter btn-load-more ${filterProduct.length < productShow ? "disabled" : ""}`} onClick={ProductShowHandler}>{filterProduct.length < productShow ? "No More Data" : "Load more"}</button>
+                    </div>
+                </div>
+            </div>
+        </Section>
     );
 }
  
