ac778d89342af7505baefc4db110d23e15bb2192
tech-aatienda <techteam@aatienda.com>
2024-11-13 04:06:30 +0400

    skjgsksdsassj

--- src/pages/api/create-checkout-session.js
+++ src/pages/api/create-checkout-session.js
@@ -1,6 +1,7 @@
-// File: /pages/api/create-checkout-session.js
 import Stripe from 'stripe';
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         const { cartItems, totalAmount } = req.body;
@@ -9,20 +10,21 @@ export default async function handler(req, res) {
             const lineItems = cartItems.map((item) => ({
                 price_data: {
                     currency: 'aed',
-                    product_data: {
-                        name: item.title,
-                    },
+                    product_data: { name: item.title },
                     unit_amount: item.price * 100, // Amount in cents
                 },
                 quantity: item.cartQuantity,
             }));
 
+            // Use req.headers.origin if available, otherwise use a default base URL
+            const baseUrl = req.headers.origin || 'https://www.holster-uae.com';
+
             const session = await stripe.checkout.sessions.create({
                 payment_method_types: ['card'],
                 line_items: lineItems,
                 mode: 'payment',
-                success_url: `${req.headers.origin}/checkout/success`,
-                cancel_url: `${req.headers.origin}/checkout/cancel`,
+                success_url: `${baseUrl}/checkout/success`,
+                cancel_url: `${baseUrl}/checkout/cancel`,
             });
 
             res.status(200).json({ url: session.url });
