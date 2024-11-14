49e40f3a6cd8eb64dc4b1cb72494e7dcd48c09ed
tech-aatienda <techteam@aatienda.com>
2024-11-14 18:33:05 +0400

    ssadhgisuddradsadscd

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,24 +1,35 @@
-// pages/api/login.js
+import clientPromise from '@/lib/mongodb';
+import bcrypt from 'bcrypt';
+
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         const { email, password } = req.body;
 
-        // Example login info for demonstration
-        const loginInfo = {
-            email: "admin@email.com",
-            password: "1234"
-        };
+        try {
+            const client = await clientPromise;
+            const db = client.db();
+            const usersCollection = db.collection('users');
 
-        // Check if the provided credentials match
-        if (email === loginInfo.email && password === loginInfo.password) {
+            // Check if user exists with the provided email
+            const user = await usersCollection.findOne({ email });
+            if (!user) {
+                return res.status(401).json({ error: 'Invalid email or password' });
+            }
+
+            // Compare the provided password with the stored hashed password
+            const isPasswordMatch = await bcrypt.compare(password, user.password);
+            if (!isPasswordMatch) {
+                return res.status(401).json({ error: 'Invalid email or password' });
+            }
+
+            // If the login is successful, send a success message
             res.status(200).json({ message: 'Login successful' });
-        } else {
-            res.status(401).json({ error: 'Invalid email or password' });
+        } catch (error) {
+            console.error("Error during login:", error);
+            res.status(500).json({ error: 'Login failed due to server error' });
         }
     } else {
-        // Handle non-POST requests
         res.setHeader('Allow', ['POST']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
     }
 }
-
