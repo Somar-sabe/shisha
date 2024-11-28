7308d26bd936a435bfdf5e9865807b83b00921ee
tech-aatienda <techteam@aatienda.com>
2024-11-28 14:53:16 +0400

    gadssdasaaS

--- src/pages/api/saveOrder.js
+++ src/pages/api/saveOrder.js
@@ -1,7 +1,4 @@
-import clientPromise from '@/lib/mongodb'; // Import the client promise
-import nodemailer from 'nodemailer'; // Import Nodemailer
-import { google } from 'googleapis'; // Import Google APIs for OAuth2
-import { OAuth2Client } from 'google-auth-library';
+import nodemailer from 'nodemailer';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
@@ -16,41 +13,13 @@ export default async function handler(req, res) {
     }
 
     try {
-      req.body.orderDate = new Date();
-      req.body.status = "Processing";
-      const client = await clientPromise;
-      const db = client.db("Shisha");
-      const ordersCollection = db.collection("orders");
-
-      // Insert the order into the database
-      const result = await ordersCollection.insertOne(req.body);
-
-      // Initialize OAuth2 client using environment variables
-      const oauth2Client = new OAuth2Client(
-        process.env.GOOGLE_CLIENT_ID, // Use environment variable for client ID
-        process.env.GOOGLE_CLIENT_SECRET, // Use environment variable for client secret
-        'https://www.holster-uae.com/saveOrder' // Your redirect URI
-      );
-
-      // Set the tokens (you would have received these tokens after the OAuth authorization flow)
-      oauth2Client.setCredentials({
-        refresh_token: process.env.GOOGLE_REFRESH_TOKEN // Use environment variable for refresh token
-      });
-
-      // Get the access token using the refresh token
-      const { token } = await oauth2Client.getAccessToken();
-
-      // Create a Nodemailer transporter using the OAuth2 token
+      // Create Nodemailer transporter using your Gmail credentials
       const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-          type: 'OAuth2',
-          user: process.env.EMAIL_USER, // Use environment variable for email user
-          clientId: process.env.GOOGLE_CLIENT_ID,
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-          refreshToken: process.env.GOOGLE_REFRESH_TOKEN, // The refresh token you obtained
-          accessToken: token.token, // The access token you obtained
-        },
+          user: process.env.EMAIL_USER, // Your Gmail address (e.g., 'your-email@gmail.com')
+          pass: 'crse tyut xdcq bxib' // Replace with your generated App Password
+        }
       });
 
       // Email content
@@ -75,7 +44,6 @@ export default async function handler(req, res) {
       return res.status(200).json({
         success: true,
         message: "Order saved and email sent successfully",
-        orderId: result.insertedId,
       });
     } catch (error) {
       console.error("Error saving order or sending email:", error);
