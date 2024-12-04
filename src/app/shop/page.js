dba4320011a385d74e69bc6387b90a884dbad424
tech-aatienda <techteam@aatienda.com>
2024-12-04 17:32:57 +0400

    gadsfdfdlmsdfsdsS

--- src/app/shop/page.js
+++ src/app/shop/page.js
@@ -3,13 +3,18 @@ import FooterTwo from "@/components/footer/FooterTwo";
 import HeaderFive from "@/components/header/HeaderFive";
 import ServiceTwo from "@/components/services/ServiceTwo";
 import ShopWithSidebar from "./ShopWithSidebar";
+import { useTranslation } from 'react-i18next'; 
 
 
 const Shop = () => {
+    const { t } = useTranslation(); 
     return ( 
         <>
         <HeaderFive headerCampaign />
-        <Breadcrumb activeItem="Shop" title="Explore All Products" />
+        <Breadcrumb 
+                activeItem={t('explor')}
+                title={t('explore')}
+            />
         <main className="main-wrapper">
         <ShopWithSidebar />
            
