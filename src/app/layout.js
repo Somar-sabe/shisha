7526279b00de9d7fb772f95e1fdf973bdf1214df
tech-aatienda <techteam@aatienda.com>
2024-12-04 16:53:06 +0400

    gaadsasacdscscdsS

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,40 +6,53 @@ import "slick-carousel/slick/slick-theme.css";
 import "@/styles/style.scss";
 import { CurrencyProvider } from '@/app/contexts/CurrencyContext';
 
-const  RootLayout = ({ children }) => {
+const RootLayout = ({ children }) => {
+  useEffect(() => {
+    window.bootstrap = require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
 
-	useEffect(() => {
-		window.bootstrap = require("bootstrap/dist/js/bootstrap.bundle.min.js");
-	}, []);
-	
-	return ( 
-		<html lang="en">
-			<head>
-			<link rel="stylesheet" href="/css/font-awesome.css" />
-			<link rel="preconnect" href="https://fonts.googleapis.com" />
-			<link rel="preconnect" href="https://fonts.gstatic.com" />
-			<link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400&display=swap" rel="stylesheet" />
-			            <meta charSet="utf-8" />
-            <meta name="robots" content="noindex, follow" />
-            <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-            <meta name="description" content="eTrade eCommerce React Next JS Template" />
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-            <title>{`Holster UAE - Premium Tobacco, Premium Moments`}</title>
-            <link rel="icon" href="/favicon.ico" />
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-			</head>
-			<body>
-				<Providers>
-				<CurrencyProvider>
-						{children}
-					</CurrencyProvider>
-				</Providers>
-			</body>
-		</html>
-	 );
-}
- 
+  return (
+    <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BQB515MRLF"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-BQB515MRLF');
+          `,
+        }} />
+        {/* Fonts and Styles */}
+        <link rel="stylesheet" href="/css/font-awesome.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400&display=swap"
+          rel="stylesheet"
+        />
+        <meta charSet="utf-8" />
+        <meta name="robots" content="noindex, follow" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="description"
+          content="eTrade eCommerce React Next JS Template"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <title>{`Holster UAE - Premium Tobacco, Premium Moments`}</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      </head>
+      <body>
+        <Providers>
+          <CurrencyProvider>{children}</CurrencyProvider>
+        </Providers>
+      </body>
+    </html>
+  );
+};
+
 export default RootLayout;
-
