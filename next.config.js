035f48a3338805768fced3ca7d28f26cbee2c6df
unknown <info@altairre.ae>
2026-05-12 13:00:56 +0400

    aqewdssassdasdsaaddadsdassadadassdadalllllsea

--- next.config.js
+++ next.config.js
@@ -1,16 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    turbo: false, // ⛔ إيقاف Turbopack وإجبار Webpack
+    turbo: false, // إيقاف Turbopack
   },
+
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
     return config;
   },
+
   i18n: {
-    locales: ['en', 'ar'], 
-    defaultLocale: 'en'
-  }
+    locales: ['en', 'ar'],
+    defaultLocale: 'en',
+  },
+
+  images: {
+    unoptimized: true, // 🔥 هذا هو الحل لمشكلتك في الإنتاج
+  },
 };
 
 module.exports = nextConfig;
